8b91754301bc059dcd832d6f5557d4370c451605
Jegans03 <jegan0345@gmail.com>
2024-07-20 17:32:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,3 @@ ScrollReveal().reveal('.home-img, .services-container, .skills-box, .contact for
 ScrollReveal().reveal('.home-contact h1, .about-img', { origin:'left' });
 ScrollReveal().reveal('.home-contact p, .about-content', { origin:'right' });
   
-  
-/*---------------Message submission------------*/
-/*const scriptURL = 'https://script.google.com/macros/s/AKfycbw4Jkc_8n_KT5EsvqqVa-ion2N92OArkYHoLG6uImNm818yEx9Z0SGxxn1_68xlVxEy4A/exec' 
-const form = document.forms['submit-to-google-sheet'] form.addEventListener('submit', e => { e.preventDefault() fetch(scriptURL, { method: 'POST', body: new FormData(form)}) .then(response => console.log('Success!', response)) .catch(error => console.error('Error!', error.message)) })*/
